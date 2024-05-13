e8b2251b1879ee40eda5bb944871227ac31c05ae
mgsvtts <mgsvtts1@gmail.com>
2024-05-13 21:06:54 +0300

    start of deposits

---
+++ src/Bonder.Deposits/Domain/Deposit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain;
+public sealed class Deposit
+{
+    public float Rate { get;  }
+    public Term Term { get; }
+
+}
+
+public readonly record struct Term(TermType Type, int Value);
+
+public enum TermType
+{
+    Day,
+    Month,
+    Year
+}
